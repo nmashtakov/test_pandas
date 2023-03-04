054c9b87f12a170f986ea0d7235917ce856cb478
Nikita Mashtakov <nik_1612@mail.ru>
2023-03-04 21:20:28 +0300

    Решение задачи 1 (без правильного оформления)

---
+++ count.py
@@ -0,0 +1,26 @@
+"""
+Описание столбцов:
+ACCOUNT_RK – номер договора
+INTERNAL_ORG_ORIGINAL_RK – номер точки продажи (POS)
+LOAN_AMOUNT – сумма кредита
+APPLICATION_DT – дата
+
+ Необходимо написать скрипт на Python, который посчитает и выведет:
+- сумму выдач в разрезе дат и точек продаж (даты и номера точек отсортированы по возрастанию)
+- процент суммы выдач в разрезе дат и точек от общей суммы выдач в дату
+
+В реализации можно использовать любые библиотеки, но достаточно csv и datetime.
+"""
+import pandas as pd
+
+df = pd.read_csv('data.csv', delimiter=';')
+df1 = df.copy(deep=True)
+#df1 = df1.query('INTERNAL_ORG_ORIGINAL_RK == -1')
+df1['percentPOS'] = df1['LOAN_AMOUNT'] / df1.groupby(['APPLICATION_DT'])['LOAN_AMOUNT'].transform('sum')
+print(pd.pivot_table(df1,
+               index=['APPLICATION_DT'],
+               columns=['INTERNAL_ORG_ORIGINAL_RK'],
+               values=['LOAN_AMOUNT', 'percentPOS'],
+               aggfunc={'LOAN_AMOUNT': 'sum', 'percentPOS': 'mean'},
+               fill_value=0
+               ))
